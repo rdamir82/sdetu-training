bb762cb0cf3d60e51e6a2dcdcf01ffb5a937ed28
Damir <rdamir82@gmail.com>
2019-01-27 11:50:04 +0100

    Dealership solution

--- src/carDealership/Dealership.java
+++ src/carDealership/Dealership.java
@@ -3,12 +3,8 @@ package carDealership;
 public class Dealership {
 
 	public static void main(String[] args) {
-		Customer cust1 = new Customer();
-		
-		cust1.setName("Tom");
-		cust1.setAddress("123 Anything St.");
-		cust1.setCashOnHand(12000);
-		
+		Customer cust1 = new Customer("Tom", "123 Anything St.", 12000);
+				
 		Vehicle vehicle = new Vehicle("Ford", "T-Model", 16000);
 		
 		Employee emp = new Employee();
