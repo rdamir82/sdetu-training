e3cfe4e87e4b296755e8cfe42de1e0db85553384
Damir <rdamir82@gmail.com>
2019-02-07 06:53:36 +0100

    threads pattern improved

---
+++ src/producerConsumerPatternImproved/Producer.java
@@ -0,0 +1,32 @@
+package producerConsumerPatternImproved;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable {
+
+	int questionNo;
+	BlockingQueue<Integer> questionQueue;
+
+	public Producer(BlockingQueue<Integer> questionQueue) {
+		this.questionQueue = questionQueue;
+	}
+
+	@Override
+	public void run() {
+
+		while (true) {
+			try {
+				synchronized (this) {
+					int nextQuestion = questionNo++;
+					System.out.println("Got new Question: " + nextQuestion);
+					questionQueue.put(nextQuestion);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
